005953cfcfd89961840b02ebd373f9cd954d0f2b
Travis Ricks <travis@travisricks.com>
2020-03-03 14:32:41 +0900

    add test image, update readme

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>oneMoon</Text>
+      <Image
+        style={{ width: 500, height: 300 }}
+        source={{
+          uri: "https://source.unsplash.com/collection/190727/1600x900"
+        }}
+      />
     </View>
   );
 }
